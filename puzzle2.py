c7111fd58929f95f47a961db69c2749bd36911bc
adriiancorral <adriancorralruiz2000@gmail.com>
2020-10-06 18:27:45 +0200

    Puzzle 2.1

--- puzzle2.py
+++ puzzle2.py
@@ -1,34 +1,48 @@
 # Libraries
 import gi
+from lcd_display import lcd
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-textbuffer = ""
+# Variables
+screen = lcd()
 
 # Code
 class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Puzzle 2")
         self.set_border_width(8)
-        #self.set_default_size(400, 200)
+        self.set_default_size(200, 50)
 
-        self.grid = Gtk.Grid()
+        self.grid = Gtk.Box(orientation="vertical", spacing=4)
         self.add(self.grid)
 
         self.textView = Gtk.TextView()
-
-        self.button1 = Gtk.ToggleButton(label="Display Text")
-        self.button1.connect("clicked", self.on_button1_clicked)
+        self.button1 = Gtk.Button(label="Display Text")
+        self.button1.connect("clicked", self.button1_clicked)
         
-        self.grid.add(self.textView)
-        self.grid.attach(self.button1, 0, 1, 1, 1)
+        self.grid.pack_start(self.textView, True, True, 0)
+        self.grid.pack_start(self.button1, True, True, 0)
 
-    def on_button1_clicked(self, widget):
+    def button1_clicked(self, widget):
         self.textbuffer = self.textView.get_buffer()
-        print(self.textbuffer)
+        self.buff = self.textbuffer.get_text(self.textbuffer.get_start_iter(), self.textbuffer.get_end_iter(), True)
+        self.textView.set_buffer()
+        
+        screen.clear()
+        self.buff_split = self.buff.split("\n")
+        self.counter = 1
+        for strings in self.buff_split:
+            screen.display_string(strings[0:20], self.counter)
+            self.counter = self.counter + 1
+            if self.counter > 4:
+                break
 
-win = MyWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+try:
+    win = MyWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+finally:
+    screen.clear()
